9dd3e579daf8bdcdae62a6b5939da567d1de8c32
tieic002 <tim.eichholz002@stud.fh-dortmund.de>
2021-01-19 23:21:35 +0100

    add bspwm and ormolu for haskell

---
+++ modules/desktop/bspwm.nix
@@ -0,0 +1,52 @@
+{ options, config, lib, pkgs, ... }:
+
+with lib;
+with lib.my;
+let cfg = config.modules.desktop.bspwm;
+in {
+  options.modules.desktop.bspwm = {
+    enable = mkBoolOpt false;
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [
+      lightdm
+      dunst
+      libnotify
+      (polybar.override {
+        pulseSupport = true;
+        nlSupport = true;
+      })
+    ];
+
+    services = {
+      xserver = {
+        enable = true;
+        displayManager = {
+          defaultSession = "none+bspwm";
+          lightdm.enable = true;
+          lightdm.greeters.mini.enable = true;
+        };
+        windowManager.bspwm.enable = true;
+      };
+    };
+
+    systemd.user.services."dunst" = {
+      enable = true;
+      description = "";
+      wantedBy = [ "default.target" ];
+      serviceConfig.Restart = "always";
+      serviceConfig.RestartSec = 2;
+      serviceConfig.ExecStart = "${pkgs.dunst}/bin/dunst";
+    };
+
+    # link recursively so other modules can link files in their folders
+    home.configFile = {
+      "sxhkd".source = "${configDir}/sxhkd";
+      "bspwm" = {
+        source = "${configDir}/bspwm";
+        recursive = true;
+      };
+    };
+  };
+}
--- modules/dev/haskell.nix
+++ modules/dev/haskell.nix
@@ -18,6 +18,7 @@ in {
     user.packages = with pkgs; [
       stack
       hlint # linter
+      ormolu # formatting
     ];
   };
 }
