de53a1eff5c99f9abaf80932c46ca5b5f4c26efd
Tim Eichholz <tim.eichholz98@web.de>
2021-02-18 16:08:17 +0000

    kitty

--- modules/desktop/apps/kitty.nix
+++ modules/desktop/apps/kitty.nix
@@ -15,7 +15,7 @@ in {
 
     home.configFile = {
       "kitty" = { source = "${configDir}/kitty"; recursive = true; };
-      "kitty/theme.conf" = { source = "${configDir}/kitty/kitty-themes/Alucard.conf"; };
+      "kitty/theme.conf" = { source = "${configDir}/kitty/kitty-themes/themes/Alucard.conf"; };
     };
   };
 }
