7034c7a5b00684fdb24c0e2f9c32f2974bfa713f
Tim Eichholz <tim.eichholz98@web.de>
2021-04-14 17:31:12 +0000

    more

---
+++ modules/dev/java.nix
@@ -0,0 +1,16 @@
+{ config, options, lib, pkgs, ... }:
+
+with lib;
+with lib.my;
+let cfg = config.modules.dev.java;
+in {
+  options.modules.dev.java = {
+    enable = mkBoolOpt false;
+  };
+
+  config = mkIf cfg.enable {
+    user.packages = with pkgs; [
+      jdk11
+    ];
+  };
+}
