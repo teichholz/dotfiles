b63ec24847d448d38846363c450c67a32f319905
Tim Eichholz <tim.eichholz98@web.de>
2021-05-27 10:45:45 +0000

    Add tex

---
+++ modules/dev/tex.nix
@@ -0,0 +1,17 @@
+{ config, options, lib, pkgs, my, ... }:
+
+with lib;
+with lib.my;
+let cfg = config.modules.dev.tex;
+in {
+  options.modules.dev.tex = {
+    enable = mkBoolOpt false;
+  };
+
+  config = mkIf cfg.enable {
+    user.packages = with pkgs; [
+      texlab
+      texlive.combined.scheme-medium
+    ];
+  };
+}
