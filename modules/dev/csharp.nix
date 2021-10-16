2cf02596eef853dc3ac11d60c9fdf10b1b4289c1
Tim Eichholz <tim.eichholz98@web.de>
2021-10-16 17:06:15 +0000

    add c#

---
+++ modules/dev/csharp.nix
@@ -0,0 +1,17 @@
+{ config, options, lib, pkgs, ... }:
+
+with lib;
+with lib.my;
+let cfg = config.modules.dev.csharp;
+in {
+  options.modules.dev.csharp = {
+    enable = mkBoolOpt false;
+  };
+
+  config = mkIf cfg.enable {
+    user.packages = with pkgs; [
+      mono
+      jetbrains.rider
+    ];
+  };
+}
