13121b0cf6cde7afcfb0479007c73ccfed32804b
Tim Eichholz <tim.eichholz98@web.de>
2021-10-17 15:09:13 +0000

    add godot-mono

---
+++ overlays/godot-mono.nix
@@ -0,0 +1,40 @@
+self: super: {
+  godot-mono = with super;
+  let
+    arch = "64";
+    version = "3.3.2";
+    releaseName = "stable";
+    subdir = "";
+    pkg = stdenv.mkDerivation  {
+      name = "godot-mono-unwrapped";
+      buildInputs = [ unzip ];
+      unpackPhase = "unzip $src";
+      version = version;
+      src = fetchurl {
+        url = "https://downloads.tuxfamily.org/godotengine/${version}${subdir}/mono/Godot_v${version}-${releaseName}_mono_x11_${arch}.zip";
+        sha256 = "sha256-imyJ/oYjZ++EeEdN99JrUuBapMr40PSuGWcuRTEc0Qw=";
+      };
+      installPhase = ''
+        cp -r . $out
+      '';
+    };
+  in buildFHSUserEnv {
+    name = "godot-mono";
+    targetPkgs = pkgs: (with pkgs;
+      [ alsaLib
+        dotnetCorePackages.sdk_5_0
+        libGL
+        libpulseaudio
+        libudev
+        xorg.libX11
+        xorg.libXcursor
+        xorg.libXext
+        xorg.libXi
+        xorg.libXinerama
+        xorg.libXrandr
+        xorg.libXrender
+        zlib
+      ]);
+    runScript = "${pkg.outPath}/Godot_v${version}-${releaseName}_mono_x11_${arch}/Godot_v${version}-${releaseName}_mono_x11.${arch}";
+  };
+}
